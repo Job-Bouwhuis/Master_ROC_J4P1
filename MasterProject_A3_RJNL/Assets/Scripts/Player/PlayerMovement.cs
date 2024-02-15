3169b92490d065f71559505f9c0eeb58ea27d635
RubenHutten <92306534+RubenHuttenROC@users.noreply.github.com>
2024-02-15 13:14:20 +0100

    WIP Character Movement without camera controll

---
+++ MasterProject_A3_RJNL/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Refrences
+    private Rigidbody rb;
+    // Variables
+    const int DISTANCE_TO_WALL = 1;
+    [SerializeField] private int baseSpeed;
+    [SerializeField] private int movementSpeedModifier;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    
+    void FixedUpdate()
+    {
+        UpdateMovement();
+    }
+
+    private void UpdateMovement()
+    {
+        Vector3 moveDir = Vector3.zero;
+        moveDir += transform.forward * Input.GetAxisRaw("Vertical");
+        moveDir += transform.right * Input.GetAxisRaw("Horizontal");
+        Vector3.Normalize(moveDir);
+
+        if (moveDir != Vector3.zero && !CheckIfMovementBlocked(moveDir))
+            rb.AddForce(moveDir * (baseSpeed + movementSpeedModifier));
+    }
+
+    bool CheckIfMovementBlocked(Vector3 moveDir)
+    {
+        Ray ray = new Ray(transform.position, moveDir);
+        Physics.Raycast(ray, out RaycastHit hitData);
+        if (hitData.distance <= DISTANCE_TO_WALL && hitData.distance != 0)
+            return true;
+        else return false;
+    }
+}
