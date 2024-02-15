caf6596280e7e90603610f8d6a45bdeb5a1791b8
L-tenThije-S <93128827+L-tenThije-S@users.noreply.github.com>
2024-02-15 13:06:15 +0100

    Begin AI

---
+++ MasterProject_A3_RJNL/Assets/Scripts/AIMovementChecker.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIMovementChecker : MonoBehaviour
+{
+    Vector3 currentPos;
+    Vector3 lastPos;
+
+    public Action onAIMoving = delegate { };
+    public Action onAIStanding = delegate { };
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentPos = transform.position;
+        if (currentPos != lastPos)
+        {
+            lastPos = currentPos;
+            onAIMoving();
+        }
+        else
+            onAIStanding();
+            
+    }
+
+}
