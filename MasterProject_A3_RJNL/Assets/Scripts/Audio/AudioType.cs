47be23aaa1ea1434799ec6778356cf5b59143921
nelis <niels.kouwer@outlook.com>
2024-03-01 14:31:48 +0100

    Start Enemy Audio Detection
    Created AudioManager, Enemy Audio Detection, an enum for audio types and audio container in order to start working on the functionality

---
+++ MasterProject_A3_RJNL/Assets/Scripts/Audio/AudioType.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AudioType
+{
+    playerWalk,
+    playerCrouch,
+    playerSprint,
+    playerShoot,
+    playerReload,
+    playerHeal,
+    playerGrabItem,
+    playerWorldInteract,
+}
