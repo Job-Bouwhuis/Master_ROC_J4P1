064c6990f64488f1074e183d71b8d01d9204cfcb
Job <0326531@student.rocvantwente.nl>
2024-03-04 16:35:25 +0100

    code cleanup V2

--- MasterProject_A3_RJNL/Assets/Scripts/UI/MouseStateOnScenePrep.cs
+++ MasterProject_A3_RJNL/Assets/Scripts/UI/MouseStateOnScenePrep.cs
@@ -1,28 +1,30 @@
+// Creator: Job
 using ShadowUprising;
 using ShadowUprising.UI.Loading;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class MouseStateOnScenePrep : MonoBehaviour, IScenePrepOperation
+namespace ShadowUprising.ScenePreps
 {
-    public CursorLockMode mode = CursorLockMode.None;
-    public bool showCursor = false;
-
-    public bool IsComplete { get; set; }
-
-    public void FinishPrep() { }
-
-    public void StartPrep()
+    public class MouseStateOnScenePrep : MonoBehaviour, IScenePrepOperation
     {
-        Cursor.lockState = mode;
-        Cursor.visible = showCursor;
+        public CursorLockMode mode = CursorLockMode.None;
+        public bool showCursor = false;
 
-        Log.Push("Mouse state switched to: " + mode.ToString());
-    }
+        public bool IsComplete { get; set; }
 
-    public YieldInstruction Update()
-    {
-        return new Completed();
+        public void FinishPrep() { }
+
+        public void StartPrep()
+        {
+            Cursor.lockState = mode;
+            Cursor.visible = showCursor;
+
+            Log.Push("Mouse state switched to: " + mode.ToString());
+        }
+
+        public YieldInstruction Update()
+        {
+            return new Completed();
+        }
     }
 }
