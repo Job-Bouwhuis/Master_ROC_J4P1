63064833ad4c1dec19da99c1d8beea5268c1b6ac
L-tenThije-S <93128827+L-tenThije-S@users.noreply.github.com>
2024-03-05 13:42:38 +0100

    changed reload animation and added particle to pistol

---
+++ MasterProject_A3_RJNL/Assets/PistolParticleHandler.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace ShadowUprising.WeaponBehaviour
+{
+    public class PistolParticleHandler : MonoBehaviour
+    {
+        /// <summary>
+        /// particles to be played
+        /// </summary>
+        public ParticleSystem particles;
+
+        /// <summary>
+        /// plays the partciles
+        /// </summary>
+        public void PlayParticles()
+        {
+            particles.Play();
+        }
+    }
+}
