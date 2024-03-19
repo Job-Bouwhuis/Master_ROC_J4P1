3e85992c1e43dfa61c6a1f8dc4e9d3504089b41c
L-tenThije-S <93128827+L-tenThije-S@users.noreply.github.com>
2024-03-19 10:50:57 +0100

    added physical buttons that the ai presses and fixed all the bugs meaning that the ai now walks to the button and presses it

--- MasterProject_A3_RJNL/Assets/Scripts/AI/AIAttackHandler.cs
+++ MasterProject_A3_RJNL/Assets/Scripts/AI/AIAttackHandler.cs
@@ -46,12 +46,6 @@ namespace ShadowUprising.AI
             state.SetState(AIState.Roaming);
         }
 
-        void SetGuardState()
-        {
-            if (state.CurrentState != AIState.Attacking)
-                state.SetState(AIState.Attacking);
-        }
-
         void CheckForReturnToRoaming()
         {
             if (IsAIAtLocation())
