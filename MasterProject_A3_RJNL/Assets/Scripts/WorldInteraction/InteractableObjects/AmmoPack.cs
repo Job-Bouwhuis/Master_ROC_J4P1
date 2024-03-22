39a9f99055ac2b4880512100783f0c6859ba87cd
RubenHutten <92306534+RubenHuttenROC@users.noreply.github.com>
2024-03-22 11:28:02 +0100

    IngameUI Prefab and unfinished AmmoPack and HealthPack components

---
+++ MasterProject_A3_RJNL/Assets/Scripts/WorldInteraction/InteractableObjects/AmmoPack.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ShadowUprising.WorldInteraction.InteractableItems
+{
+    public class AmmoPack : MonoBehaviour, IWorldInteractable
+    {
+        /// <summary>
+        /// The priority of the object for the player interaction
+        /// </summary>
+        public int Priority => 0;
+
+        private void Start()
+        {
+            Assign();
+        }
+
+        void Assign()
+        {
+            
+        }
+
+        /// <summary>
+        /// Function that gets called by the WorldInteractor system when the player looks at the item and presses the F key
+        /// </summary>
+        /// <param name="interactor">The WorldInteractor component that called this function</param>
+        public void Interact(WorldInteractor interactor)
+        {
+            // Update Ammo count
+            DestroySelf();
+        }
+
+        /// <summary>
+        /// Function called that destroys the current object
+        /// </summary>
+        public void DestroySelf()
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
