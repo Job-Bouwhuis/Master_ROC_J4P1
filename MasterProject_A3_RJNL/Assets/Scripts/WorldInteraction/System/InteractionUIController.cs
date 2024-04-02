8b89f2a3409a089da3e01c58c98e612ae08e1a3f
Job <0326531@student.rocvantwente.nl>
2024-04-02 10:54:40 +0200

    added a text when the player holds a body how to drop it

--- MasterProject_A3_RJNL/Assets/Scripts/WorldInteraction/System/InteractionUIController.cs
+++ MasterProject_A3_RJNL/Assets/Scripts/WorldInteraction/System/InteractionUIController.cs
@@ -1,16 +1,25 @@
+using ShadowUprising.Player;
 using ShadowUprising.WorldInteraction;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class InteractionUIController : MonoBehaviour
 {
-    [SerializeField] WorldInteractor worldInteractor; 
+    public const string NORMAL_TEXT = "Interact (F)";
+    public const string DROP_BODY_TEXT = "Drop body (X)";
+
+    [SerializeField] WorldInteractor worldInteractor;
+    [SerializeField, Tooltip("Optional parameter. Seeks if the player is holding a dead guard, and if so, displays a message on the HUD how to drop the body")]
+    GuardHolder guardHolder;
 
     GameObject textObject;
     Image image;
+    TMP_Text text;
+    bool isGuardHeld;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +29,7 @@ public class InteractionUIController : MonoBehaviour
 
         worldInteractor.OnItemLookedAt += ShowUI;
         worldInteractor.OnItemLookedAtStopped += HideUI;
+        text = GetComponentInChildren<TMP_Text>();
     }
 
     private void HideUI()
@@ -32,5 +42,25 @@ public class InteractionUIController : MonoBehaviour
     {
         textObject.SetActive(true);
         image.enabled = true;
+    }
+
+    private void Update()
+    {
+        if (guardHolder != null)
+        {
+            if (guardHolder.HasGuard)
+            {
+                textObject.SetActive(true);
+                text.text = DROP_BODY_TEXT;
+                isGuardHeld = true;
+            }
+            else
+            {
+                text.text = NORMAL_TEXT;
+                if(isGuardHeld)
+                    textObject.SetActive(false);
+                isGuardHeld = false;
+            }
+        }
     }
 }
