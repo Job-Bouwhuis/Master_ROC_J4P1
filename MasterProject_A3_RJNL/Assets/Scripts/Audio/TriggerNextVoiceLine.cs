8e21a9091512e41fc6a00c88d4a3eb258427cb38
Job <0326531@student.rocvantwente.nl>
2024-04-09 15:46:20 +0200

    fixed voice dialogue for when the guard is being seen by the player in the tutorial level

--- MasterProject_A3_RJNL/Assets/Scripts/Audio/TriggerNextVoiceLine.cs
+++ MasterProject_A3_RJNL/Assets/Scripts/Audio/TriggerNextVoiceLine.cs
@@ -8,10 +8,15 @@ namespace ShadowUprising.Audio
     [RequireComponent(typeof(BoxCollider))]
     public class TriggerNextVoiceLine : MonoBehaviour
     {
+        bool triggered = false;
+
         private void OnTriggerEnter(Collider other)
         {
+            if (triggered) return;
+
             if (other.CompareTag("Player"))
             {
+                triggered = true;
                 VoiceTutorialManager.Instance.PlayNextVoiceLine();
                 enabled = false;
             }
